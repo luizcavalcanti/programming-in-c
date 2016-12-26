c56b0b94426e007b02eadb84ceb310c8739df058
Luiz Carlos Cavalcanti <cavalcanti.luiz@gmail.com>
2016-12-26 14:48:24 -0200

    Adds exercise 4 for chapter 3
    Signed-off-by: Luiz Carlos Cavalcanti <cavalcanti.luiz@gmail.com>

---
+++ chapter03/ex4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+  int value1, value2, result;
+
+  value1 = 87;
+  value2 = 15;
+  result = value1 - value2;
+  
+  printf("The result of %i minus %i is %i\n", value1, value2, result);
+
+  return 0;
+}
